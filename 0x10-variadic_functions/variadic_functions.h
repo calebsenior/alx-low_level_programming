485c6e8a2b4d5b42d76c3513ae81204a22d54061
calebsenior <calebmarani@gmail.com>
2022-10-19 23:55:17 -0700

    Header update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,12 @@
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+
 #include <stdarg.h>
-#include "variadic_functions.h"
-/**
- * sum_them_all - sum all.
- * @n: given number.
- * Return: the sum.
- */
-int sum_them_all(const unsigned int n, ...)
-{
-	unsigned int i, sum = 0;
-	va_list ap;
+#include <stdio.h>
 
-	va_start(ap, n);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-	for (i = 0; i < n; i++)
-		sum = sum + va_arg(ap, int);
-
-	va_end(ap);
-	return (sum);
-}
+#endif
